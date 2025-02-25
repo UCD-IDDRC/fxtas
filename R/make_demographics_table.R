e88b70089931cabf1404ad43bafa55c86ed261b9
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-24 18:27:09 -0800

    more cleanup

--- R/make_demographics_table.R
+++ R/make_demographics_table.R
@@ -22,68 +22,62 @@ make_demographics_table <- function(
       "FXTAS Stage",
       "CGG"
       # "ApoE")
-    )
-)
-{
+    )) {
 
 
-  data_to_use = data |>
-    dplyr::select(all_of(vars), Gender, `FX*`)
+  data_to_use <- data |>
+    dplyr::select(all_of(vars), "Gender", "FX*")
   # create table using gtsummary with p-value for sex difference
 
-  table_function <- function(data) {
-    data |>
-      gtsummary::tbl_summary(
-        by = `FX*`,
-        type = gtsummary::all_continuous() ~ "continuous2",
-        statistic = list(
-          gtsummary::all_continuous() ~
-            c("{mean} ({sd})", "{median} [{min}, {max}]")
-        ),
-        # round mean to 1 digit, SD to 2 digits, Median and Range to 0
-        digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
-        missing_text = "Missing"
-      )
-  }
+
 
   tbl_stat <-
     gtsummary::tbl_strata(
       data = data_to_use,
       strata = all_of(strata),
-      .tbl_fun = ~ .x |>
-        gtsummary::tbl_summary(
-          by = `FX*`,
-          type = gtsummary::all_continuous() ~ "continuous2",
-          statistic = list(
-            gtsummary::all_continuous() ~
-              c("{mean} ({sd})", "{median} [{min}, {max}]")
-          ),
-          # round mean to 1 digit, SD to 2 digits, Median and Range to 0
-          digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
-          missing_text = "Missing"
-        )
+      .tbl_fun = ~ .x |> table_function()
     )
 
   tbl_pval <- data |>
-    dplyr::select(all_of(vars), Gender) |>
-    gtsummary::tbl_summary(by = Gender, missing_text = "Missing") |>
+    dplyr::select(all_of(c(vars, "Gender"))) |>
+    gtsummary::tbl_summary(by = "Gender", missing_text = "Missing") |>
     gtsummary::add_p(
-      pvalue_fun = function(x)
-        gtsummary::style_number(x, digits = 3)
+      pvalue_fun = \(x) gtsummary::style_number(x, digits = 3)
     ) |>
-    gtsummary::modify_column_hide(columns = c(stat_1, stat_2)) |>
+    gtsummary::modify_column_hide(columns = c("stat_1", "stat_2")) |>
     gtsummary::separate_p_footnotes(
-      footnote_prefix = "p-value for significance of sex difference by")
+      footnote_prefix = "p-value for significance of sex difference by"
+    )
 
-  to_return = list(tbl_stat, tbl_pval) |>
+  to_return <- list(tbl_stat, tbl_pval) |>
     gtsummary::tbl_merge(tab_spanner = FALSE) |>
     gtsummary::bold_labels() |>
     gtsummary::modify_footnote(
       "p.value_2" = paste(
         "p-values represent tests for sex differences in",
-        "distributions of characteristics, all CGG repeat levels.")) |>
+        "distributions of characteristics, all CGG repeat levels."
+      )
+    ) |>
     format_demographics_table_as_flextable()
 
   return(to_return)
 
+}
+
+table_function <- function(data) {
+  data |>
+    gtsummary::tbl_summary(
+      by = "FX*",
+      type = gtsummary::all_continuous() ~ "continuous2",
+      statistic = list(
+        gtsummary::all_continuous() ~
+          c("{mean} ({sd})", "{median} [{min}, {max}]")
+      ),
+      # round mean to 1 digit, SD to 2 digits, Median and Range to 0
+      digits = list(
+        `Age at visit` ~ c(1, 2, 0, 0, 0),
+        CGG ~ c(1, 2, 0, 0, 0)
+      ),
+      missing_text = "Missing"
+    )
 }
