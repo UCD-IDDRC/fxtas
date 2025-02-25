00f202255a190590acfb25b1cb2837cf8ae8ed12
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-24 19:25:59 -0800

    pkgdown, lint

--- inst/examples/exm-extract_figs_from_pickle.R
+++ inst/examples/exm-extract_figs_from_pickle.R
@@ -1,11 +1,11 @@
-output_path =
+output_path <-
   fs::path_package("extdata/sim_data", package = "fxtas")
 
-if(dir.exists(output_path))
-{
-figs = extract_figs_from_pickle(
-  output_folder = output_path,
-  n = 3)
+if (dir.exists(output_path)) {
+  figs <- extract_figs_from_pickle(
+    output_folder = output_path,
+    n = 3
+  )
 
- figs
+  figs
 }
