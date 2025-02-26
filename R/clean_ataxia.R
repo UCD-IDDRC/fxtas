68085f7a4de60066d7d762effd7540f2be45a25c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-26 01:56:22 -0800

    lint

--- R/clean_ataxia.R
+++ R/clean_ataxia.R
@@ -2,40 +2,40 @@ clean_ataxia <- function(data) {
   data |>
     dplyr::mutate(
 
-      `Ataxia severity: missingness reasons` =
-        missingness_reasons.numeric(`Ataxia: severity`),
+      "Ataxia severity: missingness reasons" =
+        missingness_reasons.numeric(.data$`Ataxia: severity`),
 
       # setting missing codes as 0s:
-      `Ataxia: severity` <-
+      "Ataxia: severity" =
         dplyr::if_else(
-          condition = `Ataxia: severity` %in% c(888,999),
+          condition = .data$`Ataxia: severity` %in% c(888, 999),
           true = "0",
-          false = `Ataxia: severity`
+          false = .data$`Ataxia: severity`
         ),
 
-      `Ataxia: severity` =
-        `Ataxia: severity` |>
+      "Ataxia: severity" =
+        .data$`Ataxia: severity` |>
         clean_numeric(),
 
-      `Ataxia` =
+      Ataxia =
         if_else(
-          condition =
-            (`Ataxia: severity` %in% 0) & is.na(Ataxia),
+          condition = (.data$`Ataxia: severity` %in% 0) &
+            is.na(.data$Ataxia),
           true = "No",
-          false = `Ataxia`
+          false = .data$`Ataxia`
         ) |>
         factor(levels = c("No", "Yes")),
 
-      `Ataxia: severity` =
+      "Ataxia: severity" =
         if_else(
-        condition =
-          (Ataxia %in% "No") & is.na(`Ataxia: severity`),
-        true = 0,
-        false = `Ataxia: severity`
-      ),
+          condition = (.data$Ataxia %in% "No") &
+            is.na(.data$`Ataxia: severity`),
+          true = 0,
+          false = .data$`Ataxia: severity`
+        ),
 
-      `Ataxia: severity*` =
-        `Ataxia: severity` |>
+      "Ataxia: severity*" =
+        .data$`Ataxia: severity` |>
         factor()
 
     )
