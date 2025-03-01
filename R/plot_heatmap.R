c0a41e6a4401b0c315de4cb5ea5f0d63e370116f
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-28 18:45:57 -0800

    lint

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -6,18 +6,22 @@
 #' @returns a [ggplot2::ggplot]
 #' @export
 #'
-plot_heatmap <- function(heatmap_data, label = "Pr(marker in this position)")
-{
+plot_heatmap <- function(heatmap_data, label = "Pr(marker in this position)") {
   heatmap_data |>
-    ggplot2::ggplot(ggplot2::aes(x = x, y = y, fill = heat)) +
+    ggplot2::ggplot() +
+    ggplot2::aes(
+      x = .data$x,
+      y = .data$y,
+      fill = .data$heat
+    ) +
     ggplot2::geom_tile() +
     ggplot2::xlab("Event Order") +
     # ggplot2::scale_fill_continuous(name = "Prob.") +
-    ggplot2::theme(legend.position="bottom") +
+    ggplot2::theme(legend.position = "bottom") +
     ggplot2::ylab("Biomarker") +
     # scale_fill_distiller(palette = "RdPu", name = "Prob.") +
     # theme_ipsum() +
-    ggplot2::scale_fill_gradient(low="white", high="blue", name = label) +
+    ggplot2::scale_fill_gradient(low = "white", high = "blue", name = label) +
     ggplot2::scale_y_discrete(limits = rev) +
     ggplot2::coord_fixed()
 }
