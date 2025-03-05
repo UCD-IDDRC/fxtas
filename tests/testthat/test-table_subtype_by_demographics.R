11f6c41ea3e9a42106d1df693e9f57db9a720458
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-04 20:05:24 -0800

    might fix

--- tests/testthat/test-table_subtype_by_demographics.R
+++ tests/testthat/test-table_subtype_by_demographics.R
@@ -10,14 +10,14 @@ test_that("`table_subtype_by_demographics()` produces consistent results",
                                                footnotes_as_letters = FALSE)
 
             html_file <- tempfile(fileext = ".html")
-            if(ft |> inherits("gt_tbl"))
-            {
+
+            if (ft |> inherits("gt_tbl")) {
             gt::gtsave(ft, filename = html_file)
-            } else
-            {
+            } else {
               ft = gtsummary::as_flex_table(ft)
               flextable::save_as_html(ft, path = html_file)
             }
+
             doconv::expect_snapshot_html(
               x = html_file,
               name = "table_subtype_by_demographics",
