ad9d8fe7fadccc009646735130fe9a36779a1925
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-04 18:36:26 -0800

    more lint

--- R/get_out_of_order.R
+++ R/get_out_of_order.R
@@ -1,10 +1,19 @@
 get_out_of_order <- function(dataset)
 {
   dataset |>
-    dplyr::select(`FXS ID`, `Visit Date`, `Event Name`) |>
-    group_by(`FXS ID`) |>
-    dplyr::filter(is.unsorted(`Event Name`)) |>
-    # dplyr::filter(any(`decreased age`, na.rm = TRUE)) |>
+    dplyr::select(
+      all_of(
+        c(
+          "FXS ID",
+          "Visit Date",
+          "Event Name")
+      )
+    ) |>
+    dplyr::filter(
+      .by = "FXS ID",
+      is.unsorted(`Event Name`)
+    ) |>
+    # dplyr::filter(.by = "FXS ID", any(`decreased age`, na.rm = TRUE)) |>
     # ungroup() |>
-    group_split()
+    group_split(across(all_of("FXS ID")))
 }
