fb882f1c7b794afc7353354eefcd2d7531fcf958
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-03-15 01:27:02 -0700

    more decomp

--- R/create_scid_domains.R
+++ R/create_scid_domains.R
@@ -22,23 +22,22 @@ create_scid_domains <- function(
   current_vars <- setdiff(scid_vars, lifetime_vars)
   # split scid_vars into the domains #
   # mood disorders
-  scid_md_vars <- scid_vars[grepl("\\(MD", scid_vars)]
+  scid_md_vars <- grep("\\(MD", scid_vars, value = TRUE)
   # substance use disorders
-  scid_sud_vars <- scid_vars[grepl("\\(SUD", scid_vars)]
+  scid_sud_vars <- grep("\\(SUD", scid_vars, value = TRUE)
   # anxiety
-  scid_anx_vars <- scid_vars[grepl("\\(ANX", scid_vars)]
+  scid_anx_vars <- grep("\\(ANX", scid_vars, value = TRUE)
   # somatization
-  scid_somatic_vars <- scid_vars[grepl("\\(SOM", scid_vars)]
+  scid_somatic_vars <- grep("\\(SOM", scid_vars, value = TRUE)
   # psychotic
-  scid_psych_vars <- scid_vars[grepl("\\(PS", scid_vars)]
+  scid_psych_vars <- grep("\\(PS", scid_vars, value = TRUE)
 
   # remove 'current' variables from each domain
-  scid_md_vars_lif <- scid_md_vars[scid_md_vars %in% lifetime_vars]
-  scid_sud_vars_lif <- scid_sud_vars[scid_sud_vars %in% lifetime_vars]
-  scid_anx_vars_lif <- scid_anx_vars[scid_anx_vars %in% lifetime_vars]
-  scid_somatic_vars_lif <-
-    scid_somatic_vars[scid_somatic_vars %in% lifetime_vars]
-  scid_psych_vars_lif <- scid_psych_vars[scid_psych_vars %in% lifetime_vars]
+  scid_md_vars_lif <- scid_md_vars |> intersect(lifetime_vars)
+  scid_sud_vars_lif <- scid_sud_vars |> intersect(lifetime_vars)
+  scid_anx_vars_lif <- scid_anx_vars |> intersect(lifetime_vars)
+  scid_somatic_vars_lif <- scid_somatic_vars |> intersect(lifetime_vars)
+  scid_psych_vars_lif <- scid_psych_vars |> intersect(lifetime_vars)
 
   dataset |>
     # convert scid_vars to ordered factors
@@ -55,14 +54,10 @@ create_scid_domains <- function(
     ) |>
     # create domain variables using max level
     dplyr::mutate(
-      # notes: could use `do.call()`, `invoke()`, `rowwise() & max()`;
-      # no clearly most-idiomatic approach as of 2024-03-22
-      # some discussion here:
-      # https://r4ds.hadley.nz/numbers.html#numeric-transformations
-      `SCID: mood disorders` = pmax(!!!rlang::syms(scid_md_vars_lif), na.rm = TRUE),
-      `SCID: substance use disorders` = pmax(!!!rlang::syms(scid_sud_vars_lif), na.rm = TRUE),
-      `SCID: anxiety disorders` = pmax(!!!rlang::syms(scid_anx_vars_lif), na.rm = TRUE),
-      `SCID: somatoform disorders` = pmax(!!!rlang::syms(scid_somatic_vars_lif), na.rm = TRUE),
-      `SCID: psychotic symptoms` = pmax(!!!rlang::syms(scid_psych_vars_lif), na.rm = TRUE)
+      `SCID: mood disorders` = rowMax(scid_md_vars_lif),
+      `SCID: substance use disorders` = rowMax(scid_sud_vars_lif),
+      `SCID: anxiety disorders` = rowMax(scid_anx_vars_lif),
+      `SCID: somatoform disorders` = rowMax(scid_somatic_vars_lif),
+      `SCID: psychotic symptoms` = rowMax(scid_psych_vars_lif)
     )
 }
