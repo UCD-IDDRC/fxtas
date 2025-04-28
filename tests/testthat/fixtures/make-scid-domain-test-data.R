045782034244e9e655db3dc30f1739265255a7ab
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-04-28 15:02:05 -0700

    more

--- tests/testthat/fixtures/make-scid-domain-test-data.R
+++ tests/testthat/fixtures/make-scid-domain-test-data.R
@@ -1,7 +1,12 @@
+devtools::load_all()
 file_path <- testthat::test_path("fixtures", "gp34_raw.rds")
 gp34_raw <- readr::read_rds(file_path)
-gp34_raw |>
-  distinct(across(all_of(all_scid_vars))) |>
+library(dplyr)
+scid_domain_test_data <-
+  gp34_raw |>
+  distinct(across(all_of(all_scid_vars)))
+
+scid_domain_test_data |>
   readr::write_rds(
   testthat::test_path("fixtures", "scid-domain-test-data.rds")
 )
