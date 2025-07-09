209cd9633e36730605698ec0608438121b0d87ba
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-07-09 12:58:37 -0700

    decomp

--- R/pvd_lineplot.R
+++ R/pvd_lineplot.R
@@ -28,100 +28,15 @@ pvd_lineplot <- function(
     y_title_size = 9,
     y_text_size = 8,
     x_text_size = 8) {
-  if (length(figs) == 1) {
-    # extract data from pvd fig object
-    dataset <- dplyr::bind_rows(figs[[1]]$data, .id = "facet")
-  } else {
-    # update list names
-    names(figs) <- facet_labels
-    # extract data from list of pvd fig object
-    dataset <- dplyr::bind_rows(
-      lapply(
-        figs,
-        function(x) {
-          x$data
-        }
-      ),
-      .id = "facet"
-    ) |>
-      dplyr::mutate(
-        facet = .data$facet |> factor(levels = names(figs))
-      )
-  }
 
-  # ---------------------------------------------------------------
-
-
-  # ---------------------------------------------------------------
-
+  dataset <- extract_lineplot_data(figs, facet_labels)
 
   # additional processing
   plot_dataset <- dataset |>
-    dplyr::mutate(
-      # extract order number
-      Order =
-        .data$`row number and name` |>
-        stringi::stri_extract_first_regex("[0-9]+") |>
-        as.integer(),
-      # right justify left facet, left justify right facet
-      hjust = ifelse(
-        .data$facet == facet_labels[1],
-        1,
-        0
-      ),
-      # made FXTAS Stage label bold
-      `event label` = ifelse(
-        .data$biomarker == "FXTAS Stage",
-        paste0("<b>", .data$`event label`, "</b>"),
-        as.character(.data$`event label`)
-      )
-    ) |>
-    dplyr::select(
-      all_of(
-        c(
-          "event name",
-          "facet",
-          "Order",
-          "biomarker",
-          "group_color",
-          "event label",
-          "hjust"
-        )
-      )
-    ) |>
-    unique() |>
-    arrange(.data$`event name`, .data$facet) |>
-    dplyr::mutate(
-      # logical: did sequence change
-      Changed = n_distinct(.data$Order) != 1,
-      # magnitude of sequence change
-      Change = -base::diff(.data$Order),
-      .by = "event name"
-    ) |>
-    dplyr::mutate(
-      linesize = ifelse(
-        .data$biomarker == "FXTAS Stage",
-        1.5,
-        1
-      ),
-      facet_order = case_when(
-        facet == facet_labels[1] ~ 1,
-        facet == facet_labels[2] ~ 1.15
-      ),
-      # colors of choice
-      Change_color = dplyr::case_when(
-        `biomarker` == "FXTAS Stage" ~ -2,
-        Change < 0 ~ -1,
-        Change == 0 ~ 0,
-        Change > 0 ~ 1,
-      ) |>
-        factor(levels = c(-2, -1, 0, 1)),
-      background = dplyr::if_else(
-        condition = .data$`event name` %in% events_to_highlight,
-        true = highlight_color,
-        false = NA
-      )
-    )
+    pvd_lineplot_preprocessing(
+    facet_labels,
+    events_to_highlight,
+    highlight_color)
 
   # alpha scaling #
   max_order <- max(plot_dataset$Order)
---
+++ R/pvd_lineplot_processing.R
@@ -0,0 +1,72 @@
+pvd_lineplot_preprocessing <- function(
+    data,
+    facet_labels,
+    events_to_highlight,
+    highlight_color) {
+  data |>
+    dplyr::mutate(
+      # extract order number
+      Order =
+        .data$`row number and name` |>
+        stringi::stri_extract_first_regex("[0-9]+") |>
+        as.integer(),
+      # right justify left facet, left justify right facet
+      hjust = ifelse(
+        .data$facet == facet_labels[1],
+        1,
+        0
+      ),
+      # made FXTAS Stage label bold
+      `event label` = ifelse(
+        .data$biomarker == "FXTAS Stage",
+        paste0("<b>", .data$`event label`, "</b>"),
+        as.character(.data$`event label`)
+      )
+    ) |>
+    dplyr::select(
+      all_of(
+        c(
+          "event name",
+          "facet",
+          "Order",
+          "biomarker",
+          "group_color",
+          "event label",
+          "hjust"
+        )
+      )
+    ) |>
+    unique() |>
+    arrange(.data$`event name`, .data$facet) |>
+    dplyr::mutate(
+      # logical: did sequence change
+      Changed = n_distinct(.data$Order) != 1,
+      # magnitude of sequence change
+      Change = -base::diff(.data$Order),
+      .by = "event name"
+    ) |>
+    dplyr::mutate(
+      linesize = ifelse(
+        .data$biomarker == "FXTAS Stage",
+        1.5,
+        1
+      ),
+      facet_order = case_when(
+        facet == facet_labels[1] ~ 1,
+        facet == facet_labels[2] ~ 1.15
+      ),
+      # colors of choice
+      Change_color = dplyr::case_when(
+        `biomarker` == "FXTAS Stage" ~ -2,
+        Change < 0 ~ -1,
+        Change == 0 ~ 0,
+        Change > 0 ~ 1,
+      ) |>
+        factor(levels = c(-2, -1, 0, 1)),
+      background = dplyr::if_else(
+        condition = .data$`event name` %in% events_to_highlight,
+        true = highlight_color,
+        false = NA
+      )
+    )
+}
