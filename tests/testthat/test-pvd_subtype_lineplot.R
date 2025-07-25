5757d9ab80c6b0bdb9ee9bc88f2b4f03059782f1
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-07-24 23:23:38 -0700

    test

---
+++ tests/testthat/test-pvd_subtype_lineplot.R
@@ -0,0 +1,18 @@
+test_that("results are consistent", {
+  output_path =
+    fs::path_package("extdata/sim_data", package = "fxtas")
+
+  figs = extract_figs_from_pickle(
+    output_folder = output_path,
+    n = 4)
+
+  events_to_highlight <- tibble::tribble(
+    ~event_name, ~line_color, ~highlight_color,
+    "Biomarker 2: 1",  "orange", "#FFF")
+
+
+  figs |>
+    pvd_subtype_lineplot(events_to_highlight = events_to_highlight) |>
+    vdiffr::expect_doppelganger(title = "sim-data-4")
+
+})
