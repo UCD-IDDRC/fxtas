4d6babb55b3dd34a7b3b0c5591ad76225b4e20b8
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-07-26 13:42:01 -0700

    update snapshot

--- tests/testthat/test-plot_compact_pvd.R
+++ tests/testthat/test-plot_compact_pvd.R
@@ -2,8 +2,10 @@ test_that("`plot_compact_pvd()` produces consistent results", {
   fig1 <-
     fs::path_package("extdata/sim_data/", package = "fxtas") |>
     extract_figs_from_pickle(output_folder = _,
-                             n_s = 3) |>
-    plot_compact_pvd(scale_colors = c("red", "blue", "purple4"))
+                             n_s = 3,
+                             use_rds = FALSE) |>
+    plot_compact_pvd(scale_colors = c("red", "blue", "purple4"),
+                     facet_label_prefix = NULL)
 
   fig1 |> vdiffr::expect_doppelganger(title = "pvd")
 
