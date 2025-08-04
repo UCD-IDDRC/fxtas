8eb06a2a0f6aefae311a4860c90ec0b94114d177
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-08-03 23:49:18 -0700

    more cleanup

--- R/label_hx_vars.R
+++ R/label_hx_vars.R
@@ -2,7 +2,7 @@ label_hx_vars <- function(data) {
   data |>
     mutate(
       across(
-        .cols = dplyr::matches(" ?hx ?"),
+        .cols = tidyselect::matches(" ?hx ?"),
         .fns =
           ~ set_label_attribute(
             .x,
