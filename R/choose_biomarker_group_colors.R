2eafc8a579955fd6c1b60e018131089effc31e0a
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-08-14 17:07:16 -0700

    add test and vector names to group colors

--- R/choose_biomarker_group_colors.R
+++ R/choose_biomarker_group_colors.R
@@ -1,18 +1,21 @@
 choose_biomarker_group_colors <- function(
     biomarker_groups,
     biomarker_group_names = names(biomarker_groups)) {
+
+  biomarker_group_colors <-
+    length(biomarker_group_names) |>
+    # rainbow() # nolint
+    # RColorBrewer::brewer.pal(n = _, name = "Paired")  # nolint
+    rcartocolor::carto_pal(n = _, "Safe") |>
+  # colorspace::qualitative_hcl(n = _, "Dark2")  # nolint
+    setNames(biomarker_group_names)
+
+  biomarker_group_colors[biomarker_group_names == "Stage"] = "black"
+
   tibble(
-    biomarker_group = biomarker_group_names
-  ) |>
-    dplyr::mutate(
-      group_color = if_else(
-        .data$biomarker_group == "Stage",
-        "black",
-        # rainbow() # nolint
-        # RColorBrewer::brewer.pal(n = n(), name = "Paired")  # nolint
-        rcartocolor::carto_pal(n = dplyr::n(), "Safe"),
-        # colorspace::qualitative_hcl(n = dplyr::n(), "Dark2")  # nolint
-      )
-    )
+    biomarker_group = biomarker_group_names |>
+      setNames(biomarker_group_colors),
+    group_color = biomarker_group_colors
+  )
 
 }
---
+++ tests/testthat/_snaps/choose_biomarker_group_colors.md
@@ -0,0 +1,11 @@
+# results are consistent
+
+    structure(list(biomarker_group = c("#88CCEE" = "Tremors", "#CC6677" = "Ataxia", 
+    black = "Stage", "#117733" = "Parkinson's disease", "#332288" = "MRI", 
+    "#AA4499" = "Scores", "#44AA99" = "SCID", "#999933" = "CANTAB", 
+    "#888888" = "Thyroid"), group_color = c(Tremors = "#88CCEE", 
+    Ataxia = "#CC6677", Stage = "black", "Parkinson's disease" = "#117733", 
+    MRI = "#332288", Scores = "#AA4499", SCID = "#44AA99", CANTAB = "#999933", 
+    Thyroid = "#888888")), class = c("tbl_df", "tbl", "data.frame"
+    ), row.names = c(NA, -9L))
+
